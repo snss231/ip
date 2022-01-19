a5c779a66461ae05dea7058cec0fa0ac0b62d3bb
snss231 <e0543670@u.nus.edu>
2022-01-19 15:51:18 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,28 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+
+    static ArrayList<String> items = new ArrayList<>();
+
     public static void main(String[] args) {
 
         greet();
 
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
+        loop: while (true) {
             String command = sc.nextLine();
-            if (command.equals("bye")) {
-                sayBye();
-                break;
-            } else {
-                echo(command);
+            switch(command) {
+                case "bye":
+                    sayBye();
+                    break loop;
+                case "list":
+                    listItems();
+                    break;
+                default:
+                    addItem(command);
+                    break;
             }
         }
     }
@@ -26,8 +35,15 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    static void echo(String command) {
-        System.out.println(command);
+    static void addItem(String command) {
+        items.add(command);
+        System.out.println("added: " + command);
+    }
+
+    static void listItems() {
+        for (int i = 0; i < items.size(); i++) {
+            System.out.println((i + 1) + ". " + items.get(i));
+        }
     }
 
 }
