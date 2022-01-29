a16f6ca43921f244ed81ce881b26a2010d9122e0
snss231 <e0543670@u.nus.edu>
2022-01-29 16:23:52 +0800

    Add basic date functionality for Deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.time.LocalDate;
 
 enum Command {
     LIST,
@@ -78,7 +80,14 @@ public class Duke {
                     break;
                 }
                 String description = parsedDeadline[0];
-                String deadline = parsedDeadline[1];
+                LocalDate deadline;
+                try {
+                    deadline = LocalDate.parse(parsedDeadline[1]);
+                } catch (DateTimeParseException e) {
+                    System.out.println("Sorry, I don't understand the date: \"" + parsedDeadline[1] + "\". " +
+                            "Enter your date in the format yyyy-mm-dd.");
+                    continue;
+                }
                 addDeadline(description, deadline);
                 break;
             }
@@ -153,7 +162,7 @@ public class Duke {
                 + "\nNow you have " + tasks.size() + " tasks in the list.");
     }
 
-    static void addDeadline(String description, String dueDateTime) {
+    static void addDeadline(String description, LocalDate dueDateTime) {
         Deadline deadline = new Deadline(description, dueDateTime);
         tasks.add(deadline);
         System.out.println("Got it. I've added this task:\n\t"
