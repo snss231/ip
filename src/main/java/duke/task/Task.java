339d188dfd89d64dd6da45918633381a91c640c9
snss231 <e0543670@u.nus.edu>
2022-01-29 22:27:01 +0800

    Add javadocs for tasks

--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -2,6 +2,9 @@ package duke.task;
 
 import java.io.Serializable;
 
+/**
+ * Abstract class representing a task with a description.
+ */
 public abstract class Task implements Serializable {
 
     protected String description;
@@ -11,14 +14,24 @@ public abstract class Task implements Serializable {
         this.description = description;
     }
 
+    /**
+     * Marks the task as completed.
+     */
     public void mark() {
         this.isDone = true;
     }
 
+    /**
+     * Unmarks task i.e. mark task as not completed.
+     */
     public void unmark() {
         this.isDone = false;
     }
 
+    /**
+     * Represents whether the task is completed or not.
+     * @return "X" if completed, " " if not completed.
+     */
     public String getStatusIcon() {
         return isDone ? "X" : " ";
     }
