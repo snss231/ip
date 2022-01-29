637c05628f8a361961ecd9c33096e92a95227819
snss231 <e0543670@u.nus.edu>
2022-01-29 22:22:06 +0800

    Add javadocs for commands

--- src/main/java/duke/command/DeadlineCommand.java
+++ src/main/java/duke/command/DeadlineCommand.java
@@ -7,16 +7,34 @@ import duke.task.Deadline;
 
 import java.time.LocalDate;
 
+/**
+ * Represents a command that creates a new Deadline task.
+ */
 public class DeadlineCommand extends Command {
 
     private LocalDate deadline;
     private String description;
     public static final String COMMAND_WORD = "deadline";
 
+    /**
+     * Creates a new DeadlineCommand.
+     *
+     * @param description Description of deadline.
+     * @param deadline Date representing the deadline.
+     */
     public DeadlineCommand(String description, LocalDate deadline) {
         this.description = description;
         this.deadline = deadline;
     }
+
+    /**
+     * Creates a new deadline task, adds it to the list of tasks, stores the tasks into the user's storage and
+     * notifies the user upon successful completion.
+     *
+     * @param tasks List of user's tasks.
+     * @param ui Handles interactions with the user.
+     * @param storage Handles storage of tasks in the user's memory.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         Deadline dl = new Deadline(description, deadline);
--- src/main/java/duke/command/EventCommand.java
+++ src/main/java/duke/command/EventCommand.java
@@ -5,6 +5,9 @@ import duke.TaskList;
 import duke.Ui;
 import duke.task.Event;
 
+/**
+ * Represents a command that creates a new Event task.
+ */
 public class EventCommand extends Command {
 
     private String description;
@@ -12,10 +15,25 @@ public class EventCommand extends Command {
 
     public static final String COMMAND_WORD = "event";
 
+    /**
+     * Creates a new EventCommand.
+     *
+     * @param description Description of deadline.
+     * @param at String representing the time that the event occurs.
+     */
     public EventCommand(String description, String at) {
         this.description = description;
         this.at = at;
     }
+
+    /**
+     * Creates a new event task, adds it to the list of tasks, stores the tasks into the user's storage and notifies the
+     * user upon successful completion.
+     *
+     * @param tasks List of user's tasks.
+     * @param ui Handles interactions with the user.
+     * @param storage Handles storage of tasks in the user's memory.
+     */
 
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
